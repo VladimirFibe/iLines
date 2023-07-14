d3d4b0d376edf6ff32590d8777eb3ec3f7114f00
Vladimir Fibe <motiw@icloud.com>
2023-07-14 14:47:39 +0600

    Initial Commit

---
+++ iLines/iLinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iLinesApp.swift
+//  iLines
+//
+//  Created by Vladimir Fibe on 14.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct iLinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
